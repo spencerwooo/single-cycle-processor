eb15a2f39ebb395ffd307d9d9d8e126fb15801ce
spencerwoo <spencerwoo98@foxmail.com>
2019-08-27 19:38:05 +0800

    init: init repo

---
+++ single-cycle-cpu.srcs/sources_1/new/pc.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2019/08/27 19:26:55
+// Design Name: 
+// Module Name: pc
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module pc(
+
+    );
+endmodule
