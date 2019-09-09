6cf4f96b7f9c202ac1d8453acd0d4c37c2e1f1f5
spencerwooo <spencerwoo98@foxmail.com>
2019-09-09 20:39:53 +0800

    update: adapt cpu to use ip catalog code

--- single-cycle-cpu.srcs/sources_1/new/register_file.v
+++ single-cycle-cpu.srcs/sources_1/new/register_file.v
@@ -6,16 +6,21 @@
 
 module register_file(
            input wire        clk,
-           input wire        reg_write,      // "Register Write" signal
+           input wire        reg_write,       // "Register Write" signal
+ 
+           input wire[4:0]   read_reg1_addr,  // Register rs address
+           input wire[4:0]   read_reg2_addr,  // Register rt address
+           input wire[4:0]   write_reg_addr,  // "Write" target register address
+           input wire[31:0]  write_data,      // "Write" target register data
+ 
+           output wire[31:0] reg1_data,       // Register rs data
+           output wire[31:0] reg2_data,       // Register rt data
 
-           input wire[4:0]   read_reg1_addr, // Register rs address
-           input wire[4:0]   read_reg2_addr, // Register rt address
-           input wire[4:0]   write_reg_addr, // "Write" target register address
-           input wire[31:0]  write_data,     // "Write" target register data
-
-           output wire[31:0] reg1_data,      // Register rs data
-           output wire[31:0] reg2_data       // Register rt data
+           output wire[7:0]  debug_reg_single // Debug signal
        );
+
+// Debug signal output
+assign debug_reg_single = gpr[1][7:0];
 
 // General purpose register
 reg[31:0] gpr[31:0];
