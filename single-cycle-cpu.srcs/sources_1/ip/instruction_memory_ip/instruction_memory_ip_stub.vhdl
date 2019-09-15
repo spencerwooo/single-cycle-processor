63187f355459f5d1482f4a53c7116b2c8a4080d1
spencerwooo <spencerwoo98@foxmail.com>
2019-09-15 16:12:47 +0800

    tests: generation passed

---
+++ single-cycle-cpu.srcs/sources_1/ip/instruction_memory_ip/instruction_memory_ip_stub.vhdl
@@ -0,0 +1,31 @@
+-- Copyright 1986-2017 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2017.2 (win64) Build 1909853 Thu Jun 15 18:39:09 MDT 2017
+-- Date        : Sun Sep 15 15:45:43 2019
+-- Host        : DESKTOP-IT0A8CI running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub -rename_top instruction_memory_ip -prefix
+--               instruction_memory_ip_ instruction_memory_ip_stub.vhdl
+-- Design      : instruction_memory_ip
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7a35tcsg324-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity instruction_memory_ip is
+  Port ( 
+    a : in STD_LOGIC_VECTOR ( 9 downto 0 );
+    spo : out STD_LOGIC_VECTOR ( 31 downto 0 )
+  );
+
+end instruction_memory_ip;
+
+architecture stub of instruction_memory_ip is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "a[9:0],spo[31:0]";
+attribute x_core_info : string;
+attribute x_core_info of stub : architecture is "dist_mem_gen_v8_0_11,Vivado 2017.2";
+begin
+end;
